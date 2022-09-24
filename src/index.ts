ccbc7f7559703faf05946f06412f714c9edb0364
moussa D <moussa.doumbia9219@gmail.com>
2022-09-24 17:09:21 +0000

    setup type-orm

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { createConnection } from "typeorm";
 
-const app = express();
+createConnection().then(() => {
+  const app = express();
 
-app.use(express.json());
+  app.use(express.json());
 
-app.use(cors({
-  origin: ['http://localhost:4200']
-}))
+  app.use(cors({
+    origin: ['http://localhost:4200']
+  }))
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('hello world');
+  app.get('/', (req: Request, res: Response) => {
+    res.send('hello world');
+  })
+
+  app.listen(3000, () => {
+    console.log('listening on port 3000');
+  })
 })
 
-app.listen(3000, () => {
-  console.log('listening on port 3000');
-})
