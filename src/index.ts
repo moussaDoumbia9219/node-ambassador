31381619b0467f90ad704ec33c71d4e57e904e6f
moussa D <moussa.doumbia9219@gmail.com>
2022-09-24 16:06:46 +0000

    configure express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello Here 2");
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors({
+  origin: ['http://localhost:4200']
+}))
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('hello world');
+})
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+})
